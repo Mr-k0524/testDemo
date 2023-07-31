b40b2e4e7ad91934f94c168e426498734bf37da3
ksh <shenghangk@gmail.com>
2023-07-31 22:54:40 +0800

    Revert "test"
    This reverts commit ce230050760419a00f2f2da8b40565f266ed41c9.

--- src/main/java/com/example/testdemo/controller/HelloController.java
+++ src/main/java/com/example/testdemo/controller/HelloController.java
@@ -1,14 +1,2 @@
-package com.example.testdemo.controller;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-@Controller
-public class HelloController {
-    @RequestMapping("/hello")
-    @ResponseBody
-    public String hello() {
-        return "Hello World";
-    }
+package com.example.testdemo.controller;public class HelloController {
 }
