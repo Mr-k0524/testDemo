1a14dc669411b4c6f4238f30c60c9e09f6114cc9
ksh <shenghangk@gmail.com>
2023-07-31 22:54:40 +0800

    Revert "Create README.md"
    This reverts commit 29368733582ec2e4a7ca0ecf2a0bfb57f6c0cbe6.

--- src/main/java/com/example/testdemo/controller/HelloController.java
+++ src/main/java/com/example/testdemo/controller/HelloController.java
@@ -1,2 +1,12 @@
-package com.example.testdemo.controller;public class HelloController {
+package com.example.testdemo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @RequestMapping("hello")
+    public String hello(){
+        return "Hello World";
+    }
 }
